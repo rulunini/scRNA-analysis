f9a98938970c8940c37917a4fd5a0b8c610f9cb9
rulunini <sohee5172@gmail.com>
2023-04-14 10:32:08 +0900

    Update Celltype_dimplot.R Changed the name of Celltype_dimplot.R to Dimplot_as_celltype.R

--- Celltype_cluster_number_to_Visium.R
+++ Celltype_cluster_number_to_Visium.R
@@ -4,22 +4,45 @@ library(Seurat)
 
 # < Prepare your data > ----------
 
+my.path <- 'write_your_path'
+
 # This is Load your visium dataset 
-load('/home/write_your_path/spatial_eachsample_seperated.RData')
+load(paste0(my.path, 'spatial_eachsample_seperated.RData'))
 # This is .csv file that already created to identify enriched celltypes from divided clusters at visium 
-my.df <- read.csv('/home/write_your_path/enrichment_signif_celltype.csv')
+my.df <- read.csv(paste0(my.path, 'enrichment_signif_celltype.csv'))
 
 
 # < Create variables > -----------
 
 # Convert individual visium data to list format
-my.visium.lst <- list(tm1, tm2, ln1, ln2, tm3, ln3)
+my.visium.lst <- list(tm1, ln1, tm2, ln2, tm3, ln3)
 
 my.celltype <- colnames(my.df)
 my.celltype <- paste0(my.celltype, '.enrich')
 
 
 # < Just run the code! > ----------
+
+
+for (i in 1:length(my.visium.lst)){
+  
+  for (z in 1:length(my.celltype)){
+
+  # Create new metadata that include my resolution
+  my.visium.lst[[i]]$new <- my.visium.lst[[i]]$integrated_snn_res.1
+  my.visium.lst[[i]]$new <- my.visium.lst[[i]]$integrated_snn_res.1[my.visium.lst[[i]]$integrated_snn_res.1 %in% as.numeric(na.omit(my.df[,z]))]
+  colnames(my.visium.lst[[i]]@meta.data)[length(colnames(my.visium.lst[[i]]@meta.data))] <- my.celltype[z]
+  
+  
+  my.visium.lst[[i]]@meta.data[[my.celltype[z]]][my.visium.lst[[i]]@meta.data[[my.celltype[z]]] %in% as.numeric(na.omit(my.df[,z]))] <- '1'
+  my.visium.lst[[i]]@meta.data[[my.celltype[z]]][my.visium.lst[[i]]@meta.data[[my.celltype[z]]] %in% NA] <- '0'
+  my.visium.lst[[i]]@meta.data[[my.celltype[z]]] <- factor(my.visium.lst[[i]]@meta.data[[my.celltype[z]]])
+  Idents(my.visium.lst[[i]]) <- my.celltype[z]
+  
+  
+  }
+}
+
 
 # If the cluster is under my celltype to "1", or not "0"
 for (i in 1:length(my.visium.lst)){
@@ -40,5 +63,6 @@ for (i in 1:length(my.visium.lst)){
 
 # < Save to RData > ----------
 
-my.path <- '/home/write_your_path'
-save(my.visium.lst, file = paste0(my.path, '/spatial_eightsample_integration.RData'))
+save.path <- 'write_your_path'
+save(my.visium.lst, file = paste0(save.path, '/spatial_sixsample_integration_celltype.RData'))
+
